e392552b5cd66202f93978fa17d6c08a7966f73e
Nathan Smith <nathansforyou@gmail.com>
2019-06-22 23:06:26 -0400

    level 2 complete

--- django/level_2/level_2_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    my_dict = {'insert_content': "Welcome to lvl 2sies"}
-    return render(request,'level_2_app/index.html',context=my_dict)
--- django/second_project/app_two/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, re_path
-from app_two import views
-
-urlpatterns = [
-    re_path(r'^$',views.help,name='help'),
-]
